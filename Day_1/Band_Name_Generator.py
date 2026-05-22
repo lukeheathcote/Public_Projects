feab93aa4595436e96b33fb24d02eca41ad27291
Luke Heathcote <76099634+lukeheathcote@users.noreply.github.com>
2026-05-22 19:19:02 +0100

    Day 1
    Day 1 - A fancy band name generator

---
+++ Day_1/Band_Name_Generator.py
@@ -0,0 +1,7 @@
+#Day 1 Band Name Generator
+print("Lets make you an awesome band name!")
+Location = input("Where did you grow up? ")
+print("wow really? " + Location + "! thats cool!")
+Pet_Name = input("What is a good pet name? ")
+print("yeah maybe " + Pet_Name + " works.")
+print("How about " + Location + Pet_Name + "?")
