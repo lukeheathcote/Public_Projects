31d2efc5fd4b6e73c9bd2230ba45d4a06fd6d9de
Luke Heathcote <76099634+lukeheathcote@users.noreply.github.com>
2026-05-27 08:20:16 +0100

    Day 2
    Tip Calculator Project

---
+++ Day_2/Tip_Calculator.py
@@ -0,0 +1,10 @@
+#Tip Calculator
+print("welcome to the tip calculator!" )
+bill = float(input("What was the total bill? £"))
+tip = int(input("How much tip would you like to give? "))
+split = int(input("How many people would you like to split the bill? "))
+#inputs done, Maths Next
+bill_amount = bill / 100 * tip / split
+totalbill = round(bill_amount, 2)
+print(f"Each person should pay: {totalbill}")
+
